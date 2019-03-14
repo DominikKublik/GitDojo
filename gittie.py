5efd25f0f1575ebbe4c3de5fb413b86f8b9eca1e
JanAbczynski <jan.abczynski@gmail.com>
2019-03-14 10:01:38 +0100

    new funk

--- gittie.py
+++ gittie.py
@@ -1,16 +1,25 @@
 def hello():
     print("Hello, I'm Gettie!")
 
+
 def add():
     print("add")
 
+
 hello()
 hello()
 
+
 def joke():
-    x=1
-    while True:        
+    x = 1
+    while True:
         print(x)
-        x+=1
+        x += 1
+
+
+def introduct():
+    print('blabla')
+    print('blablabla')
+
 
 joke()
